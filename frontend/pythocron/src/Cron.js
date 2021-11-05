0806da16d12c205694afa86553ffb1bf37317ae0
Oskar Korczak <oskarissimus@gmail.com>
2021-11-05 20:33:11 +0100

    refactor cron to functional component

--- frontend/pythocron/src/Cron.js
+++ frontend/pythocron/src/Cron.js
@@ -7,70 +7,68 @@ import CronExample from "./CronExample"
 import { styled } from '@mui/material/styles';
 import cronstrue from 'cronstrue';
 
-class Cron extends React.Component {
-    handleCronExpressionUpdate = event => {
-        this.props.onCronExpressionUpdate(event.target.value)
+
+
+export default function Cron(props) {
+
+    const handleCronExpressionUpdate = event => {
+        props.onCronExpressionUpdate(event.target.value)
     }
-    handleCronExpressionExampleClicked = cronExpression => {
-        this.props.onCronExpressionUpdate(cronExpression)
-        // console.log("CIPSKO")
+    const handleCronExpressionExampleClicked = cronExpression => {
+        props.onCronExpressionUpdate(cronExpression)
     }
-    render() {
 
-        const CronExpressionInput = styled(TextField)({
-            '& .MuiInputBase-input': {
-                fontSize: 40,
-                fontFamily: "monospace",
-                textAlign: "center"
-            },
-            '& .MuiFormHelperText-root': {
-                fontSize: 20,
-                fontFamily: "monospace",
-                textAlign: "center"
-            }
-        });
+    const CronExpressionInput = styled(TextField)({
+        '& .MuiInputBase-input': {
+            fontSize: 40,
+            fontFamily: "monospace",
+            textAlign: "center"
+        },
+        '& .MuiFormHelperText-root': {
+            fontSize: 20,
+            fontFamily: "monospace",
+            textAlign: "center"
+        }
+    });
 
+    return (
+        <Stack spacing={3}>
+            <Typography variant="h2" sx={{ textAlign: "center" }} >
+                Cron
+            </Typography>
 
-        return (
-            <Stack spacing={3}>
-                <Typography variant="h2" sx={{ textAlign: "center" }} >
-                    Cron
-                </Typography>
+            <CronExpressionInput
+                label="Cron expression"
+                variant="outlined"
+                sx={{ width: 1, fontFamily: "monospace", fontSize: 200 }}
+                value={props.cronExpression}
+                onChange={handleCronExpressionUpdate}
+                helperText={cronstrue.toString(props.cronExpression, { verbose: true, use24HourTimeFormat: true })}
+            />
+            <Typography variant="h5" component="h3">
+                Examples:
+            </Typography>
+            <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+                columnSpacing={1}
+            >
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="* * * * *" cronExpressionDescription="Every minute" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="*/2 * * * *" cronExpressionDescription="Every 2 minutes" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="*/10 * * * *" cronExpressionDescription="Every 10 minutes" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="0 * * * *" cronExpressionDescription="Every hour at minute 0" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="17 * * * *" cronExpressionDescription="Every hour at minute 17" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="0 0 * * *" cronExpressionDescription="Every day at 00:00" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="37 21 * * *" cronExpressionDescription="Every day at 21:37" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="37 21 * * 1" cronExpressionDescription="Every monday at 21:37" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="37 21 2 * *" cronExpressionDescription="Every second day of each month at 21:37" />
+                <CronExample onCronExpressionExampleClicked={handleCronExpressionExampleClicked} cronExpression="37 21 2 4 *" cronExpressionDescription="Every second day of april at 21:37" />
 
-                <CronExpressionInput
-                    label="Cron expression"
-                    variant="outlined"
-                    sx={{ width: 1, fontFamily: "monospace", fontSize: 200 }}
-                    value={this.props.cronExpression}
-                    onChange={this.handleCronExpressionUpdate}
-                    helperText={cronstrue.toString(this.props.cronExpression, { verbose: true, use24HourTimeFormat: true })}
-                />
-                <Typography variant="h5" component="h3">
-                    Examples:
-                </Typography>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    columnSpacing={1}
-                >
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="* * * * *" cronExpressionDescription="Every minute" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="*/2 * * * *" cronExpressionDescription="Every 2 minutes" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="*/10 * * * *" cronExpressionDescription="Every 10 minutes" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="0 * * * *" cronExpressionDescription="Every hour at minute 0" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="17 * * * *" cronExpressionDescription="Every hour at minute 17" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="0 0 * * *" cronExpressionDescription="Every day at 00:00" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="37 21 * * *" cronExpressionDescription="Every day at 21:37" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="37 21 * * 1" cronExpressionDescription="Every monday at 21:37" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="37 21 2 * *" cronExpressionDescription="Every second day of each month at 21:37" />
-                    <CronExample onCronExpressionExampleClicked={this.handleCronExpressionExampleClicked} cronExpression="37 21 2 4 *" cronExpressionDescription="Every second day of april at 21:37" />
-
-                </Grid>
+            </Grid>
 
 
-            </Stack>
-        )
-    }
+        </Stack>
+    )
 }
-export default Cron
