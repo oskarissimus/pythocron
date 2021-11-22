82fea427bd0ac12be700a08613a725040db9ffb2
Oskar Korczak <oskarissimus@gmail.com>
2021-11-22 19:10:42 +0100

    backend rewriten from system cron to apscheduler

--- backend/pythocron/config.py
+++ backend/pythocron/config.py
@@ -2,6 +2,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    logs_dir_path: str = "/app/data/logs"
-    scripts_dir_path: str = "/app/data/scripts"
-    crontab_path: str = "/app/data/crontab"
+    # data_dir_root: str = "/app"
+    data_dir_root: str = "/home/oskar/git/pythocron/backend/data"
+    # python_interpreter_path: str = "/usr/local/bin/python"
+    python_interpreter_path: str = "/home/oskar/.virtualenvs/pythocron/bin/python"
+
+    logs_dir_path: str = f"/{data_dir_root}/logs"
+    scripts_dir_path: str = f"/{data_dir_root}/scripts"
+    crontab_path: str = f"/{data_dir_root}/crontab"
