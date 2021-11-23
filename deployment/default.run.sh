1a5601cd56181ed64a75a61ab264e758f1464d37
Oskar Korczak <oskarissimus@gmail.com>
2021-11-23 09:58:38 +0100

    fixed generating nginx conf from template

--- deployment/default.run.sh
+++ deployment/default.run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst < /app/deployment/default.conf.template > /app/deployment/default.nginx.conf
+envsubst < /app/deployment/default.nginx.conf.template > /app/deployment/default.nginx.conf
 nginx -c /app/deployment/default.nginx.conf
 uvicorn pythocron.main:app --root-path=/api/v1 --host=0.0.0.0
